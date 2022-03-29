70e88cedfa2fd944fe10f695814562b155dab2e1
konnic <konstantin.nicolaus@gmail.com>
2022-03-29 08:41:00 +0200

    add capacitor to frontend project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.todoload.app',
+  appName: 'todoload',
+  webDir: 'dist/frontend',
+  bundledWebRuntime: false,
+};
+
+export default config;
